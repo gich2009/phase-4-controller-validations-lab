b1b3207e9ae9f5be66a1c80d82ca81ff28632fe9
gich2009 <waneneamos@gmail.com>
2023-07-08 17:19:02 +0300

    Completed assignment

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,21 +1,40 @@
 class AuthorsController < ApplicationController
+  wrap_parameters format: []
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+  rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
   
   def show
-    author = Author.find(params[:id])
-
+    author = find_author
     render json: author
   end
 
-  def create
-    author = Author.create(author_params)
 
+  def create
+    author = Author.create!(author_params)
     render json: author, status: :created
   end
+
+
 
   private
   
   def author_params
     params.permit(:email, :name)
+  end
+
+
+  def find_author
+    Author.find(params[:id])
+  end
+
+
+  def render_record_not_found exception
+    render json: { error: "#{exception.model.underscore.humanize} not found" }, status: :not_found
+  end
+
+
+  def render_unprocessable_entity exception
+    render json: {errors: exception.record.errors}, status: :unprocessable_entity
   end
   
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,20 @@
 class PostsController < ApplicationController
+  wrap_parameters format: []
+  rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
+  rescue_from ActiveRecord::RecordInvalid,  with: :render_unprocessable_entity
 
   def show
-    post = Post.find(params[:id])
-    
+    post = find_post
     render json: post
   end
+
 
   def update
-    post = Post.find(params[:id])
-
-    post.update(post_params)
-
-    render json: post
+    post = find_post
+    post.update!(post_params)
+    render json: post, status: :ok
   end
+
 
   private
 
@@ -20,4 +22,18 @@ class PostsController < ApplicationController
     params.permit(:category, :content, :title)
   end
 
+
+  def find_post
+    Post.find(params[:id])
+  end
+
+
+  def render_record_not_found exception
+    render json: { error: "#{exception.model.underscore.humanize} not found" }, status: :not_found 
+  end
+
+
+  def render_unprocessable_entity exception
+    render json: { errors: exception.record.errors }, status: :unprocessable_entity
+  end
 end
